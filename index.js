89b77e4c6441371c1ccd0279100b9ec11333097c
Bajuddin Khan <84461218+bajuddin15@users.noreply.github.com>
2023-03-06 22:13:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,22 +19,10 @@ app.use(cors());
 app.use("/api/users", userRoutes);
 app.use("/api/contact", contactRoutes);
 
-// --------------------------deployment------------------------------
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.send("API is running..");
-  });
-}
-// --------------------------deployment------------------------------
+    res.send("API is running...");
+ });
 
 const PORT = process.env.PORT || 5000;
 
